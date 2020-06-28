e024eabc29c3a636e8e425ecd372a1af9b080535
dcrocker <danielacrocker@gmail.com>
2020-06-28 19:45:10 -0400

    Form movies appears

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
+import AddMovie from '../views/AddMovie.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ Vue.use(VueRouter)
     path: '/contact',
     name: 'Contact',
     component: Contact 
+  },
+  {
+    path: '/movies/add',
+    name: 'AddMovie',
+    component: AddMovie 
   },
   ]
 
