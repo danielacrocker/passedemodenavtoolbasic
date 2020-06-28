e4108d95f94c02380abd8e6d7a5c84b3c9e499ab
dan <danielacrocker@gmail.com>
2020-06-28 08:25:41 -0400

    basic nav, toolbar installed

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav>
+      <v-toolbar>
+        <v-toolbar-title class="grey--text">
+          <span class="font-weight-light">Site</span>
+          <span>Title</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text>
+          <span>Sign Out</span>
+          <v-icon right>exit_to_app</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+      <v-navigation-drawer v-model="drawer" app class="primary">
+        <p>test</p>
+      </v-navigation-drawer>
+
+    </nav>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  }), 
+}
+</script>
