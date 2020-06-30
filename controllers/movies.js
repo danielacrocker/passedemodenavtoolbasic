1ea316d47b7c90b57416f9134bc81f64973f3010
dcrocker <danielacrocker@gmail.com>
2020-06-29 20:22:33 -0400

    able to get movies ok

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,16 @@
 const MovieSchema = require('../models/movie.js');
 
 module.exports.controller = (app) => {
+  // fetch all movies
+  app.get('/movies', (req, res) => {
+    MovieSchema.find({}, 'name description release_year genre', (error, movies) => {
+      if (error) { console.log(error); }
+      res.send({
+        movies,
+      })
+    })
+  })
+  
   // add a new movie
   app.post('/movies', (req, res) => {
     const newMovie = new MovieSchema({
